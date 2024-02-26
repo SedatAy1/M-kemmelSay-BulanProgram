8ee8bee4e09c85ca4a1cc33782635e33555c3262
Sedat Ay <159232350+SedatAy1@users.noreply.github.com>
2024-02-26 09:58:23 +0300

    MükemmelSayıBulanProgram
    MükemmelSayıBulanProgram

---
+++ MükemmelSayıBulanProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MükemmelSayıBulanProgram {
+    public static void main(String[] args) {
+        Scanner imp = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz : ");
+        int n = imp.nextInt();
+
+        int total = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                total += i;
+            }
+        }
+
+        if (total == n) {
+            System.out.println(n + " Sayısı Mükemmel Bir Sayıdır.");
+        }else {
+            System.out.println(n + " Sayısı Mükemmel Bir Sayı Değildir.");
+        }
+    }
+}
